86b742564513e2c162afada2ff3dc23ebd4f6ba4
Julia Hu <juliahukoshi@gmail.com>
2019-03-12 18:23:50 +0100

    added reset color on click

--- map/map.js
+++ map/map.js
@@ -1,11 +1,22 @@
 const map = document.querySelector("#map");
 let paths = map.querySelectorAll(".map_img a");
 let links = map.querySelectorAll(".list_map ul li a");
+let country = "";
 
 links.forEach(function (link){
     link.addEventListener("click", function(){
-        let toto = link.getAttribute('title');
-        console.log(link);
-        document.getElementById("japan").style.fill = "#3100a5";
+        if(country!= ""){
+            country.style.fill = "#75a3a3";
+            let identifier = link.getAttribute("title");
+            console.log(identifier);
+            let countryUpdate = document.getElementById(identifier);
+            countryUpdate.style.fill = "#3100a5";
+            country = countryUpdate;
+        } else {
+            let identifier = link.getAttribute("title");
+            let countryUpdate = document.getElementById(identifier);
+            countryUpdate.style.fill = "#3100a5";
+            country = countryUpdate;
+        }
     })
 })
