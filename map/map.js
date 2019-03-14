d5bcfe7d72f828bb18d6cb3a6492d7162a66acec
Julia Hu <juliahukoshi@gmail.com>
2019-03-14 11:33:44 +0100

    map modified

--- map/map.js
+++ map/map.js
@@ -20,3 +20,59 @@ links.forEach(function (link){
         }
     })
 })
+
+
+// MODAL JAPAN //
+
+var modalJapan = document.getElementById('myModalJapan');
+
+// Get the button that opens the modal
+var btnJapan = document.getElementById("myBtnJapan");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btnJapan.onclick = function() {
+  modalJapan.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalJapan.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalJapan) {
+    modalJapan.style.display = "none";
+  }
+}
+
+// MODAL MEXICO //
+
+var modalMexico = document.getElementById('myModalMexico');
+
+// Get the button that opens the modal
+var btnMexico = document.getElementById("myBtnMexico");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[1];
+
+// When the user clicks on the button, open the modal
+btnMexico.onclick = function() {
+  modalMexico.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalMexico.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalMexico) {
+    modalMexico.style.display = "none";
+  }
+}
+
